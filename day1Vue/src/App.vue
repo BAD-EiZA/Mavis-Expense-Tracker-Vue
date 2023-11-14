62ce18184dcc77b39005faabb38ba7395dc9a9ab
BAD-EiZA <badapplestd@gmail.com>
2023-11-14 13:36:38 +0700

    first commit

---
+++ day1Vue/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <HeaderComponent/>
+  <div class="container">
+    <BalanceComponent/>
+    <IncomeExpense/>
+    <TransactionList/>
+  </div>
+</template>
+
+<script>
+import BalanceComponent from './components/BalanceComponent.vue';
+import HeaderComponent from './components/HeaderComponent.vue';
+import IncomeExpense from './components/IncomeExpense.vue';
+import TransactionList from './components/TransactionList.vue';
+export default {
+  components: {
+    HeaderComponent,
+    BalanceComponent,
+    IncomeExpense,
+    TransactionList
+}
+}
+</script>
---
+++ day1Vue/src/components/HeaderComponent.vue
@@ -0,0 +1,5 @@
+<template>
+    <h2>
+        Mavis Tracker Expense
+    </h2>
+</template>
