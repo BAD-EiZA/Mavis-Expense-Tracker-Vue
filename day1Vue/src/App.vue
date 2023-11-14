a014b237a7aa0d54fe71f4ad4715af277c2dba82
BAD-EiZA <badapplestd@gmail.com>
2023-11-14 14:04:54 +0700

    Update Mapping Global State

--- day1Vue/src/App.vue
+++ day1Vue/src/App.vue
@@ -3,21 +3,27 @@
   <div class="container">
     <BalanceComponent/>
     <IncomeExpense/>
-    <TransactionList/>
+    <TransactionList :transactions="transactions"/>
+    <AddTransaction/>
   </div>
 </template>
 
-<script>
+<script setup>
+import AddTransaction from './components/AddTransaction.vue';
 import BalanceComponent from './components/BalanceComponent.vue';
 import HeaderComponent from './components/HeaderComponent.vue';
 import IncomeExpense from './components/IncomeExpense.vue';
 import TransactionList from './components/TransactionList.vue';
-export default {
-  components: {
-    HeaderComponent,
-    BalanceComponent,
-    IncomeExpense,
-    TransactionList
-}
-}
+import {ref} from 'vue'
+const transactions = ref([
+    {
+        id: 1, text: "Sword", amount: -20000
+    },
+    {
+        id: 2, text: "Shield", amount: 200000
+    },
+    {
+        id: 3, text: "Armor", amount: 600000
+    }
+]);
 </script>
--- day1Vue/src/components/TransactionList.vue
+++ day1Vue/src/components/TransactionList.vue
@@ -1,11 +1,25 @@
 <template>
-     <h3>History</h3>
-      <ul id="list" class="list">
-        <li class="minus">
+    <h3>History</h3>
+    <ul id="list" class="list">
+        <li v-for="t in transactions" :key="t.id" :class="t.amount < 0 ? 'minus' : 'plus'">
+            {{ t.text }} <span>Rp. {{ t.amount }}</span><button class="delete-btn">x</button>
+        </li>
+        <!-- <li class="minus">
           Cash <span>-$400</span><button class="delete-btn">x</button>
         </li>
         <li class="plus">
           Paycheck <span>$400</span><button class="delete-btn">x</button>
-        </li>
-      </ul>
+        </li> -->
+    </ul>
 </template>
+
+<script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps({
+  transactions: {
+    type: Array,
+    required: true
+  }
+})
+</script>
